9b755beb7cef1789fbc7eda9712e9e95eb112174
smarv2 <smarv2@hotmail.com>
2017-06-02 11:01:05 -0500

    cuarto cambio

---
+++ src/teste/Algo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+/**
+ *
+ * @author MXI01020253A
+ */
+public class Algo {
+    String hola;
+}
