a43ca5e63bda73182b0daa23e9169b5affee224e
smarv2 <smarv2@hotmail.com>
2017-06-02 11:06:51 -0500

    octavo cambio

---
+++ src/teste/Lolo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+/**
+ *
+ * @author MXI01020253A
+ */
+public class Lolo {
+    
+    String lolo = "lolo";
+    
+}
